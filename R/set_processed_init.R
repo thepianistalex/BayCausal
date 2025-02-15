7bb278a2b5d6f99c62d0b0f4ac70d29a2a1b22f5
thepianistalex <llang7@jh.edu>
2025-02-14 21:10:50 -0500

    correct argument misplacement mistake
    correct mistake
    
    correct mistake

--- R/set_processed_init.R
+++ R/set_processed_init.R
@@ -26,28 +26,28 @@ set_processed_init <- function(data, mh_setup_lst, init_lst, prior_lst,
 
 update_init_wo_UGLT <- function(init_lst, res, prior_lst){
   
-  init_lst$mu <- extract_post_mean("mu", res, init_lst$P_star)
+  init_lst$mu <- extract_post_mean(res, "mu", init_lst$P_star)
   
-  gamma_alpha_tmp <- extract_post_mean("gamma_alpha", res, init_lst$P_star)
+  gamma_alpha_tmp <- extract_post_mean(res, "gamma_alpha", init_lst$P_star)
   gamma_alpha_tmp <- ifelse(gamma_alpha_tmp > 0.5, 1, prior_lst$nu_0)
   init_lst$gamma_alpha <- gamma_alpha_tmp
-  init_lst$nu_alpha <- extract_post_mean("nu_alpha", res, init_lst$P_star)
-  init_lst$rho_alpha <- extract_post_mean("rho_alpha", res, init_lst$P_star)
-  A_tmp <- extract_post_mean("A", res, init_lst$P_star) * init_lst$gamma_alpha
+  init_lst$nu_alpha <- extract_post_mean(res, "nu_alpha", init_lst$P_star)
+  init_lst$rho_alpha <- extract_post_mean(res, "rho_alpha", init_lst$P_star)
+  A_tmp <- extract_post_mean(res, "A", init_lst$P_star) * init_lst$gamma_alpha
   init_lst$A <- A_tmp * gamma_alpha_tmp
   
-  gamma_beta_tmp <- extract_post_mean("gamma_beta", res, init_lst$P_star)
+  gamma_beta_tmp <- extract_post_mean(res, "gamma_beta", init_lst$P_star)
   gamma_beta_tmp <- ifelse(gamma_beta_tmp > 0.5, 1, prior_lst$nu_0)
-  for(q in ncol(gamma_beta_tmp)){
+  for(q in 1:ncol(gamma_beta_tmp)){
     gamma_beta_tmp[q,q] <- 0
   }
   init_lst$gamma_beta <- gamma_beta_tmp
-  init_lst$nu_beta <- extract_post_mean("nu_beta", res, init_lst$P_star)
-  init_lst$rho_beta <- extract_post_mean("rho_beta", res, init_lst$P_star)
-  B_tmp <- extract_post_mean("B", res, init_lst$P_star)
+  init_lst$nu_beta <- extract_post_mean(res, "nu_beta", init_lst$P_star)
+  init_lst$rho_beta <- extract_post_mean(res, "rho_beta", init_lst$P_star)
+  B_tmp <- extract_post_mean(res, "B", init_lst$P_star)
   init_lst$B <- B_tmp * gamma_beta_tmp
   
-  init_lst$sigma2 <- extract_post_mean("sigma2", res, init_lst$P_star)
+  init_lst$sigma2 <- extract_post_mean(res, "sigma2", init_lst$P_star)
   
   return(init_lst)
 }
