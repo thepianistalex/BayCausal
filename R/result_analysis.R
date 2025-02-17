e5dc1021f073213959ae14ee07d1d6d0fe5daa19
thepianistalex <llang7@jh.edu>
2025-02-17 11:28:51 -0500

    Export get_mode, for post processing L, do cutoff first then order

--- R/result_analysis.R
+++ R/result_analysis.R
@@ -48,6 +48,19 @@ post_process_L <- function(res, order_flag, cutoff = Inf) {
   
   processed_res <- lapply(res, function(sample) {
     
+    if (is.finite(cutoff)) {
+      sample$L[abs(sample$L) < cutoff] <- 0
+      sample$sparsity_matrix <- ifelse(sample$L == 0, 0, 1)
+      sample$pivot <- apply(sample$L, 2, function(col_j) {
+        nz <- which(col_j != 0)
+        if (length(nz) == 0) {
+          NA_integer_  
+        } else {
+          nz[1] 
+        }
+      })
+    }
+    
     if (order_flag) {
       ord <- order(sample$pivot, decreasing = FALSE)
       sample$L <- sample$L[, ord, drop = FALSE]
@@ -55,9 +68,6 @@ post_process_L <- function(res, order_flag, cutoff = Inf) {
       sample$pivot <- sample$pivot[ord]
     }
     
-    if (is.finite(cutoff)) {
-      sample$L[abs(sample$L) < cutoff] <- 0
-    }
     
     return(sample)
   })
@@ -67,6 +77,12 @@ post_process_L <- function(res, order_flag, cutoff = Inf) {
 
 
 
+#' Title
+#'
+#' @param v 
+#'
+#' @returns mode of the vector
+#' @export
 get_mode <- function(v) {
   uniqv <- unique(v)
   uniqv[which.max(tabulate(match(v, uniqv)))]
